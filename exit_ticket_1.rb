869108a1b449c1ba1f05f8863b047565da45ab96
Blythe McFall <mcfall.blythe@gmail.com>
2018-04-24 13:23:49 -0600

    Completed exercises in exit_ticket_1.rb and exit_ticket_2.rb.

--- exit_ticket_1.rb
+++ exit_ticket_1.rb
@@ -1,12 +1,43 @@
 # TO DO:
 # Create an array of six elements - content of your choosing (either integers or strings!)
+array = ["cats", "dogs", "fish", "hedgehogs", "horses"]
+# Returns=> ["cats", "dogs", "fish", "hedgehogs", "horses"]
+
 # Access the third element
+array[2]
+# Returns => "fish"
+
 # Add an element to the end of your array
+array.push("llamas")
+# Returns => ["cats", "dogs", "fish", "hedgehogs", "horses", "llamas"]
+
 # Add an element to index 2
+array.insert(2, "elephants")
+# Returns => ["cats", "dogs", "elephants", "fish", "hedgehogs", "horses", "llamas"]
+
 # Remove the first element from your array
+array.delete_at(0)
+# Returns => "cats"
+# array Returns  => ["dogs", "elephants", "fish", "hedgehogs", "horses", "llamas"]
+
 # Remove any duplicates that may exist in your array
+# Adding Duplicate Item to Array
+array.push("elephants")
+# Returns => ["dogs", "elephants", "fish", "hedgehogs", "horses", "llamas", "elephants"]
+array.uniq!
+# Returns => ["dogs", "elephants", "fish", "hedgehogs", "horses", "llamas"]
+
 # Create a second array with 3 elements
+array_1 = ["leopard cats", "mandarin ducks", "pekin robins"]
+# Returns => ["leopard cats", "mandarin ducks", "pekin robins"]
+
 # "Sqash" the two arrays together
+array.concat(array_1)
+# Returns => ["dogs", "elephants", "fish", "hedgehogs", "horses", "llamas", "leopard cats", "mandarin ducks", "pekin robins"]
 
 # BONUS since we didn't cover today 🦄
 # If your array contains strings, write a sentence using each string. If it contains integers, create a multiplication table for your array.
+puts "Most #{array[0]}, #{array[2]}, #{array[3]}, #{array[4]} and #{array[5]} are domesticated animals.  However, #{array[1]}, #{array[2]}, #{array[3]}, #{array[4]}, #{array[6]}, #{array[7]} and #{array[8]} are still found in the wild."
+
+# Returns => Most dogs, fish, hedgehogs, horses and llamas are domesticated animals.  However, elephants, fish, hedgehogs, horses, leopard cats, mandarin ducks and pekin robins are still found in the wild.
+# => nil
--- exit_ticket_2.rb
+++ exit_ticket_2.rb
@@ -14,27 +14,95 @@ pet.length
 # YOUR TURN:
 
 hedgies = ["Alvin", "Bagel", "Bella", "Ernie", "Kalua", "Margo", "Murphy", "Pino", "Spike", "Trix"]
+# Line 16 creates a new array with 10 elements.
+
 new_hedgies = ["Mooshi", "Monkey"]
+# Line 19 creates a new array with two elements.
+
 
 hedgies[3]
+# Returns the value of the element at the 3rd indexed position in the hedgies array.
+# => "Ernie"
+
 hedgies.empty?
-hedgies.at[5]
+# Checks to see if the hedgies array is empty.
+# => Returns false, as expected.
+
+#hedgies.at[5]
+hedgies.at(5)
+# I changed the brackets to parentheses to pass in the argument.
+# Returns the element in the 5th indexed position of the hedgies array.
+# => "Margo"
+
 hedgies.unshift("Bella")
+# Adds an additional "Bella" string element to the beginning of the hedgies array as the first element in the array.
+# => ["Bella", "Alvin", "Bagel", "Bella", "Ernie", "Kalua", "Margo", "Murphy", "Pino", "Spike", "Trix"]
+
 hedgies.insert(6, "Onyx")
+# Adds the "Onyx" string element to the 6th indexed position
+# => ["Bella", "Alvin", "Bagel", "Bella", "Ernie", "Kalua", "Onyx", "Margo", "Murphy", "Pino", "Spike", "Trix"]
+
 hedgies.compact
+# Removes nil objects from an array and then returns the array.
+# => ["Bella", "Alvin", "Bagel", "Bella", "Ernie", "Kalua", "Onyx", "Margo", "Murphy", "Pino", "Spike", "Trix"]
+
 hedgies.pop
+# Removes the last element in the hedgies array.
+# Returns => "Trix"
+# Mutates the hedgies array as follows:  ["Bella", "Alvin", "Bagel", "Bella", "Ernie", "Kalua", "Onyx", "Margo", "Murphy", "Pino", "Spike"]
+
 hedgies.fetch(0)
+# Returns the first element in the hedgies array at the 0 index.
+# => "Bella"
+
 hedgies.include?("Bagel")
+# In this example, we're asking the hedgies array if it includes and the string element "Bagel", which is true.
+# => true
+
 hedgies.shift
+# Here we've utilized the #shift method to remove the first element in the hedgies array.
+# => "Bella"
+
 hedgies.push("Toast")
+# In this case, we're adding the "Toast" string element via the #push method, which places the element in the last position of the hedgies array.
+# => ["Alvin", "Bagel", "Bella", "Ernie", "Kalua", "Onyx", "Margo", "Murphy", "Pino", "Spike", "Toast"]
+
 hedgies << "Rocky"
+# Here we're appending the string element "Rocky" to the end of the hedgies array.
+# => ["Alvin", "Bagel", "Bella", "Ernie", "Kalua", "Onyx", "Margo", "Murphy", "Pino", "Spike", "Toast", "Rocky"]
+
 hedgies.count
-# also, what is at least one other way to accomplish this?
+# The .count method returns the number of elements currently present in the hedgies array.
+# => 12
+# Also, what is at least one other way to accomplish this?
+# The .size method could also be utilized to complete this task.
+
 hedgies.uniq!
-# also, how would this be different if we omitted the "!"?
+# On Line #62 we shifted the duplicate item in the array off, therefore the .uniq! method returns => nil since the other items in the array are unique.
+# Also, how would this be different if we omitted the "!"?
+# If only the .uniq method was utilized the original array remains intact, whereas the .uniq! method mutates the original array and returns the altered # collection.
+
 hedgies.concat(new_hedgies)
+# The new_hedgies array elements are appended to the hedgies array.
+# => ["Alvin", "Bagel", "Bella", "Ernie", "Kalua", "Onyx", "Margo", "Murphy", "Pino", "Spike", "Toast", "Rocky", "Mooshi", "Monkey"]
 
 # BONUS since we didn't cover today 🦄
 hedgies.each do |hedgie|
   puts hedgie
 end
+# Each element in hedgies array evalutated and is then displayed in the teriminal.
+# Alvin
+# Bagel
+# Bella
+# Ernie
+# Kalua
+# Onyx
+# Margo
+# Murphy
+# Pino
+# Spike
+# Toast
+# Rocky
+# Mooshi
+# Monkey
+# Returns => ["Alvin", "Bagel", "Bella", "Ernie", "Kalua", "Onyx", "Margo", "Murphy", "Pino", "Spike", "Toast", "Rocky", "Mooshi", "Monkey"]
